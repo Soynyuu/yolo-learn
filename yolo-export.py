df740b8990784ac9fb2d18168b610fb6aea16c3c
Kodai Miyazaki | Soynyuu <117629460+Soynyuu@users.noreply.github.com>
2024-08-26 10:10:01 +0000

    fix bug??

--- yolo-export.py
+++ yolo-export.py
@@ -1,13 +1,7 @@
 from ultralytics import YOLO
 
 # Load the YOLOv8 model
-model = YOLO("yolov8n.pt")
+model = YOLO("/workspaces/yolo-learn/runs/detect/train/weights/best.pt")
 
 # Export the model to TF.js format
-model.export(format="tfjs")  # creates '/yolov8n_web_model'
-
-# Load the exported TF.js model
-tfjs_model = YOLO("./yolov8n_web_model")
-
-# Run inference
-results = tfjs_model("https://ultralytics.com/images/bus.jpg")
+model.export(format="onnx")  # creates '/yolov8n_web_model'
